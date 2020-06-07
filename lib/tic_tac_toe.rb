429002fc87b576f5eb9560546b205d114f109681
Grace Kim <goooyoon@gmail.com>
2020-06-07 04:11:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,8 @@ WIN_COMBINATIONS = [
 
   def initialize
     @board = Array.new(9, " ")
-    
+  end
+  
   def display_board 
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
